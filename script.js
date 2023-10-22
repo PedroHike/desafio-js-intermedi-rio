cf4c6b0025e041549bc28e54d857d520d24326dc
Pedro Henrique Amaral <ph.amaral1991@gmail.com>
2023-10-22 13:41:48 -0300

    Refatoração do arquivo JS e algumas no htm e css

--- script.js
+++ script.js
@@ -1,63 +1,17 @@
-const input = document.querySelectorAll('.input')
+const botao = document.getElementById('btn');
+const campoDePreenchimento = document.querySelectorAll('.input');
 
-const container = document.querySelectorAll('.container');
-
-const btnEnviar = document.querySelector('.btn-enviar')
-
-
-
-btnEnviar.addEventListener('click', (event)=>{
-
-    input.forEach((input, indice)=>{
-        const classesDoContainer = container[indice].classList
-        const inputVazio = input.value == ""
-        const inputPreenchido = input.value!==""
-
-        
-        if(inputVazio){
-            
-            verificarSeInputEstaVazio(classesDoContainer, event);
-            
-        }else if(inputPreenchido){
-            
-            verificarSeInputEstahPreenchido(classesDoContainer);
-        }
-        
-    })
+botao.addEventListener('click', (event)=>{
+        campoDePreenchimento.forEach((elemento)=>{
+                if(elemento.value){
+                        elemento.classList.add('valido');
+                        elemento.classList.remove('invalido');
+                        elemento.nextElementSibling.classList.remove('ativa')
+                }else{
+                        event.preventDefault();
+                        elemento.classList.remove('valido');
+                        elemento.classList.add('invalido');
+                        elemento.nextElementSibling.classList.add('ativa');
+                }
+        });
 })
-
-input.forEach((input, indice)=>{
-        input.addEventListener('change', ()=>{
-        const classesDoContainer = container[indice].classList
-        const inputVazio = input.value == ""
-        const inputPreenchido = input.value!==""
-
-        
-        if(inputVazio){
-            
-            verificarSeInputEstaVazio(classesDoContainer);
-            
-        }else if(inputPreenchido){
-            
-            verificarSeInputEstahPreenchido(classesDoContainer);
-        }
-        
-    })
-
-})
-
-
-function verificarSeInputEstaVazio(classesDoContainer, event) {
-    if (classesDoContainer.contains('preenchido')) {
-        classesDoContainer.remove('preenchido');
-    }
-    classesDoContainer.add('erro');
-    event.preventDefault();
-}
-
-function verificarSeInputEstahPreenchido(classesDoContainer) {
-    if (classesDoContainer.contains('erro')) {
-        classesDoContainer.remove('erro');
-    }
-    classesDoContainer.add('preenchido');
-}
